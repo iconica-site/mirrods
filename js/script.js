3d7214cf970f003357f3c6b39353c134e63afa28
Javid <cavidgulmaliyev491995@gmail.com>
2023-11-30 18:48:04 +0400

    added footer to the pages

--- js/script.js
+++ js/script.js
@@ -444,7 +444,93 @@ upButton?.addEventListener("click", () => {
   });
 });
 
+;// CONCATENATED MODULE: ./src/js/modules/move.js
+class Move {
+  #$destination;
+  #$elementAtIndex;
+  #$placeholder = document.createElement("div");
+  #$target;
+  #breakpoint;
+  #breakpointType;
+  #destinationChildren;
+  #index;
+  #matchMedia;
+
+  /** @param {MoveOptions} options */
+  constructor(options) {
+    this.#$destination = document.querySelector(options.destinationSelector);
+    this.#$target = document.querySelector(options.targetSelector);
+
+    if (this.#$destination && this.#$target) {
+      this.#breakpoint = options.breakpoint ?? 768;
+      this.#breakpointType = options.breakpointType ?? "max";
+      this.#destinationChildren = this.#$destination.children;
+      this.#index = options.index ?? "last";
+      this.#matchMedia = matchMedia(`(${this.#breakpointType}-width: ${this.#breakpoint}px)`);
+
+      if (this.#index !== "first" && this.#index !== "last") {
+        this.#$elementAtIndex = this.#destinationChildren[this.#index];
+      }
+
+      this.#init();
+    }
+  }
+
+  #init() {
+    this.#$placeholder.hidden = true;
+
+    if (this.#matchMedia.matches) this.#move();
+
+    this.#matchMedia.addEventListener("change", event => {
+      event.matches ? this.#move() : this.#remove();
+    });
+  }
+
+  #move() {
+    this.#$target.insertAdjacentElement("beforebegin", this.#$placeholder);
+
+    if (this.#$elementAtIndex) {
+      this.#$elementAtIndex.insertAdjacentElement("beforebegin", this.#$target);
+    } else if (this.#index === "first") {
+      this.#$destination.insertAdjacentElement("afterbegin", this.#$target);
+    } else {
+      this.#$destination.insertAdjacentElement("beforeend", this.#$target);
+    }
+  }
+
+  #remove() {
+    this.#$placeholder.insertAdjacentElement("beforebegin", this.#$target);
+    this.#$placeholder.remove();
+  }
+}
+
+
+
+;// CONCATENATED MODULE: ./src/js/scripts/scripts/move.js
+
+
+const footerSocials = document.querySelector(".footer-socials");
+const footerTopColumn = document.querySelector(".footer-top__column--top");
+
+if (footerSocials && footerTopColumn) {
+  const move = new Move({
+    destinationSelector: ".footer-top__column--top",
+    targetSelector: ".footer-socials",
+  });
+}
+
+const footerInfo = document.querySelector(".footer-bottom__item--info");
+const footerTopInner = document.querySelector(".footer-top__inner");
+
+if (footerInfo && footerTopInner) {
+  const move = new Move({
+    destinationSelector: ".footer-top__inner",
+    targetSelector: ".footer-bottom__item--info",
+  });
+}
+
 ;// CONCATENATED MODULE: ./src/js/scripts/scripts.js
+
 
 
 
